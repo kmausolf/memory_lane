8fc212cd1c8548a7f8fb9eeafc6ed22787d0679d
Niral Patel <ndp002@ucsd.edu>
2017-10-31 18:19:05 -0700

    Niral: Year check not calling checkYear function

--- js/memory_lane.js
+++ js/memory_lane.js
@@ -4,21 +4,43 @@
 	//define module
 	var app = angular.module('selecting', []);
 
+	var year = [
+		{thousands: 0, 
+		 hundreds: 0,
+		 tens: 0,
+		 ones: 0,
+		}
+	]
+
 	//define controller
 	//capitalcase always!
 	app.controller('CountController', function(){
 		this.count = 0;
 
-		this.increaseCount = function() {
+		this.increaseCount = function(place) {
 			if( this.count < 9 )
 				this.count += 1;
+			year[0].place = this.count;
 			
 		};
 
-		this.decreaseCount = function() {
-			if( this.count > 0 ) {
+		this.decreaseCount = function(place) {
+			if( this.count > 0 ) 
 				this.count -= 1;
-			};
+			year[0].place = this.count;
 		};
 	});
+
+	app.controller('ExploreController', function(){
+		this.currentYear = (year[0].thousands*1000 + year[0].hundreds*100 + year[0].tens*10 + year[0].ones);
+
+		this.checkYear = function(){
+			console.log(currentYear);
+			if( currentYear > 1980 ) {
+				angular.element("#submit").href = "explore2.html";
+			}
+		}
+	});
 })();
+
+
