a733ec741bf220b6f7b055ce47606ec9ceaac954
Niral Patel <ndp002@ucsd.edu>
2017-10-31 20:06:55 -0700

    Niral: Year conditions work

--- js/memory_lane.js
+++ js/memory_lane.js
@@ -5,7 +5,8 @@
 	var app = angular.module('selecting', []);
 
 	var year = [
-		{thousands: 0, 
+		{
+		 thousands: 0, 
 		 hundreds: 0,
 		 tens: 0,
 		 ones: 0,
@@ -20,26 +21,33 @@
 		this.increaseCount = function(place) {
 			if( this.count < 9 )
 				this.count += 1;
-			year[0].place = this.count;
+			year[0][place] = this.count;
 			
 		};
 
 		this.decreaseCount = function(place) {
 			if( this.count > 0 ) 
 				this.count -= 1;
-			year[0].place = this.count;
+			year[0][place] = this.count;
 		};
 	});
 
 	app.controller('ExploreController', function(){
-		this.currentYear = (year[0].thousands*1000 + year[0].hundreds*100 + year[0].tens*10 + year[0].ones);
+		this.currentYear = (year[0]['thousands']*1000 + year[0]['hundreds']*100 + year[0]['tens']*10 + year[0]['ones']);
 
+		/* Figure out why the heck currentYear evaluates to 0 in the if statement */
 		this.checkYear = function(){
-			console.log(currentYear);
-			if( currentYear > 1980 ) {
-				angular.element("#submit").href = "explore2.html";
+			console.log(year[0]['thousands']*1000 + year[0]['hundreds']*100 + year[0]['tens']*10 + year[0]['ones']);
+			
+			if( (year[0]['thousands']*1000 + year[0]['hundreds']*100 + year[0]['tens']*10 + year[0]['ones']) > 1980 ){
+				window.location = "explore2.html";
 			}
+			else {
+				window.location = "explore.html";
+			}
+			
 		}
+
 	});
 })();
 
