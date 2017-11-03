11d07e8cd98df5458aac448a7b062458a876e089
Sam Wang <yuw160@ucsd.edu>
2017-11-03 01:03:52 -0700

    explore page layout

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+/*
+(getHeight() {
+  var object = document.getElementById('content_box')
+  var width = object.width();
+  return width;
+}):
+*/
+
+
+//randomly select from a folder
+var i = Math.floor(Math.random() * 6) + 1;
+$("#my-div").load('//' + i + '.html');
+
+
+
+
+var path = '/path-to-files/',
+files = ['1.php', '2.php', '3.php', '4.php', '5.php', '6.php'],
+i = Math.floor(Math.random()*files.length);
+var url = (path+files[i]);
+$("#my-div").load(url);
