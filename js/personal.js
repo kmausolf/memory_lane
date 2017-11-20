76fccf5447b56383073091bde56a0755166f08e1
Sam Wang <yuw160@ucsd.edu>
2017-11-19 19:57:41 -0800

    made settings synchronous using promises

--- js/personal.js
+++ js/personal.js
@@ -1,74 +1,168 @@
 /*jslint devel: true */
 
+//NOTE for future reference: firebase.auth().currentUser.uid is the user's ID, unique to the Firebase project. Do NOT use this value to authenticate with your backend server, if you have one. Use User.getToken() instead.
+
 //closure for javascript
 //(function () {
 
-/****************************** Start ******************************/
+/****************************** Setup ******************************/
 
 //calls functions on page load
-$(document).ready(function(){
+//$(document).ready(function(){
 
-});
+//Tries to use previously initialized firebaseApp
+var app;
+try {
+  app = firebase.app();
+}
+//If firebaseApp has not yet been initialized, initializes it
+catch(e) {
+  console.log('initializing firebaseApp...');
+  config = {
+    //NOTE: public apiKey is a security issue
+    apiKey: "AIzaSyABzPj3JJjsth_x7iEEeus_diIj_Hx3ajg",
+    authDomain: "cse-170.firebaseapp.com",
+    databaseURL: "https://cse-170.firebaseio.com",
+    projectId: "cse-170",
+    storageBucket: "cse-170.appspot.com"
+  };
+  app = firebase.initializeApp(config);
+}
+
+//})();//end of ready function
 
 /****************************** Functions ******************************/
 
-//test function
-function testFunction() {
+//Initializes user data in firebase with the user's email and uid.
+function initializeUserData() {
 
-  console.log('getting user info...');
-  //get user info
-
-  //Initialize Firebase?
-  var app;
-  try {
-    config = {
-      //NOTE: public apiKey is a security issue
-      apiKey: "AIzaSyABzPj3JJjsth_x7iEEeus_diIj_Hx3ajg",
-      authDomain: "cse-170.firebaseapp.com",
-      databaseURL: "https://cse-170.firebaseio.com",
-      projectId: "cse-170",
-      storageBucket: "cse-170.appspot.com"
-    };
-    app = firebase.app();
-  }
-  catch(e) {
-    app = firebase.initializeApp(config);
-  }
-
-  /*
-  //lets firebase be used elsewhere
-  var firebaseApp = firebase.initializeApp(config);
-  module.exports.FBApp = firebaseApp.database();
-  */
-
-  var user = firebase.auth().currentUser;
-  var name, email, photoUrl, uid, emailVerified;
+  //Declare variables
+  var user, email, uid;
+  //Initialize variables
+  user = firebase.auth().currentUser;
   if (user != null) {
-    name = user.displayName;
+    //variables
     email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
-    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-    // this value to authenticate with your backend server, if
-    // you have one. Use User.getToken() instead.
+    uid = user.uid;
+  }
+  else {
+    console.log('User is null. Cannot initialize user data.');
+    return;
   }
 
-  console.log('printing user info...');
-  console.log(name);
-  console.log(email);
-  console.log(photoUrl);
-  console.log(emailVerified);
-  console.log(uid);
-
-
-
-
-
-
+  //writes to firebase
+  console.log('Initializing user data with:');
+  console.log('   email : ' + email);
+  console.log('   user id: ' + uid);
+  firebase.database().ref('users/' + uid).update({
+    email: email,
+    userID: uid
+  });
 }
 
+//Uses the "path" param to find the "field" in firebase
+//and sets the value of that field to "value".
+//If changing field with uid as root, use '' as the path value.
+function writeUserData(path, field, value) {
+  //test function: write user data to firebase
 
+  //Declare variables
+  var user, uid;
+  //Initialize variables
+  user = firebase.auth().currentUser;
+  if (user != null) {
+    uid = user.uid;
+  }
+  else {
+    console.log('User is null. Cannot write user data to database.');
+    return;
+  }
 
+  //writes to firebase
+  console.log('Updating user ' + field + ' field to: ' + value)
+  firebase.database().ref('users/' + uid + path).update({
+    [field]: value
+  });
+}
 
+//Returns a promise for the user data (specified by "field" param) 
+//based on the path (using the uid as the root of the path).
+function getUserData(path, field) {
 
+  //Declare variables
+  var user, email, uid;
+  //Initialize user id
+  user = firebase.auth().currentUser;
+  if (user == null) {
+    console.log('User is null. Cannot get user data.');
+    return;
+  }
+  var uid = user.uid;
+  //get firebase database reference to appropriate data in firebase
+  var ref = firebase.database().ref('/users/' + uid + path).child(field);
 
+  //Uses the reference to get the value of the appropriate element.
+  //Utilizes firebase promise to return a promise of the data
+  return ref.once('value').then(function(dataSnapshot) {
+    console.log('getUserData returned: ' + dataSnapshot.val());
+    return Promise.resolve(dataSnapshot.val());
+    //error handling if promise was rejected
+  }, function(error) {
+    console.log('The promise was rejected.');
+    console.log(error);
+    return null;
+  });
+};
+
+/****************************** Old Code ******************************/
+
+/*
+  firebase.auth().currentUser.once('value').then(function(snapshot) {
+    console.log('first part of promise');
+    console.log(snapshot.val());
+  }, function(error) {
+    console.log('error occurred while initializing app:');
+    console.log(error);
+  });
+  console.log('creating current user instance...')
+*/
+
+/*
+//fires when the user is logged in
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('User is signed in');
+  } else {
+    console.log('No user is signed in');
+  }
+});
+
+if(user == null){
+  console.log('User not yet logged in');
+}
+else {
+  console.log('getCurrentUser returned logged in');
+}
+*/
+
+/*
+  //attempt at sync function
+  var syncRef = new Firebase("https://cse-170.firebaseio.com");
+  var authData = syncRef.getAuth();
+  if (authData) {
+    console.log('synchronous call successful');
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log('synchronous call unsuccessful');
+    console.log("User is logged out");
+  }
+  */
+
+/*
+  //delay script
+  var start = new Date().getTime();
+  var end = start;
+  while(end < start + 1000) {
+    end = new Date().getTime();
+  }
+  */
--- js/settings.js
+++ js/settings.js
@@ -1,47 +1,127 @@
-(function(){
+//calls functions on page load
+$(document).ready(function(){
+
+  //Firebase promise that fires once it's determined if the user is logged in or out
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    //localStorage.clear();
+    console.log('firebase.auth().onAuthStateChanged');
+    console.log('---');
+    getSetting('music_setting', firebaseUser);
+    getSetting('movies_setting', firebaseUser);
+    getSetting('shows_setting', firebaseUser);
+
+  });
+});
+
+
+//function to get the specified setting of the user
+function getSetting(setting, user) {
+  
+  //If the user is logged in, pulls setting from database
+  if(user) {
+    //Gets promise of user settings data specified by "setting" param
+    console.log('Getting ' + setting + ' setting from database.');
+    var promise = getUserData('/settings', setting);
+    //Attempts to get value of promise
+    try{
+      promise.then(function(resolveValue) {
+        //returns setting if the promise value resulted in !null
+        if(resolveValue != null) {
+          console.log('Returning found setting for ' + setting + ': ' + resolveValue);
+          return resolveValue;
+        }
+        //else set the setting in database to true and returns true
+        else {
+          console.log(setting + ' was previous null or undefined (in database). Setting it to true.');
+          writeUserData('/settings', setting, true);
+          return true;
+        }
+        //catches promises that resulted in rejection
+      }).catch(function(error) {
+        console.log(error);
+      });
+    }
+    //logs error if attempt failed
+    catch(e) {
+      console.log('promise.then error:')
+      console.log(e);
+    }
+  }
+
+  //else if the user is not logged in, pulls the setting from localStorage
+  else {
+    console.log('User is logged out. Getting ' + setting + ' setting from localStorage.');
+    currSetting = localStorage.getItem(setting);
+    //returns setting if the setting is found in localStorage
+    if(currSetting != null) {
+      console.log('Returning found setting for ' + setting + ': ' + currSetting);
+      return currSetting;
+    }
+    //else set the setting in localStorage to be true and returns true
+    else {
+      console.log(setting + ' was previous null or undefined (in localStorage). Setting it to true.');
+      localStorage.setItem(setting, true);
+      return true;
+    }
+  }
+}
+
+
+
+/****************************** Old Code ******************************/
+
+/*
+function togglerandom(string) {
+  console.log('test function');
+}
+
+
+(function() {  
 
   var app = angular.module('settings', []);
-
   app.controller("ButtonCtrl", function($scope){
+
+    //sets the active state of the music button
     if(localStorage.getItem("show_music") === null){
       $scope.musicActive = true;
-    }//end if
+    }
     else{
       $scope.musicActive = localStorage.getItem("show_music");
-    }//end else
-
+    }
+    //sets the active state of the movies button
     if(localStorage.getItem("show_movies") === null){
       $scope.moviesActive = true;
-    }//end if
+    }
     else{
       $scope.moviesActive = localStorage.getItem("show_movies");
-    }//end else
-
+    }
+    //sets the active state of the shows button
     if(localStorage.getItem("show_shows") === null){
       $scope.showsActive = true;
-    }//end if
+    }
     else{
       $scope.showsActive = localStorage.getItem("show_shows");
-    }//end else
+    }
 
-    $scope.toggleActive = function(cat){
-      if(cat == "music"){
-        //Toggle the musicActive button, and save its state in localStorage
+    //functionality to toggle the buttons on-click
+    $scope.toggleActive = function(section){
+      //Toggle the musicActive button, and save its state in localStorage
+      if(section == "music"){
         $scope.musicActive = !$scope.musicActive;
         localStorage.setItem("show_music", $scope.musicActive);
-      }//end music if
-      else if (cat == "movies") {
-        //Toggle the moviesActive button, and save its state in localStorage
+      }
+      //Toggle the moviesActive button, and save its state in localStorage
+      else if (section == "movies") {
         $scope.moviesActive = !$scope.moviesActive;
         localStorage.setItem("show_movies", $scope.moviesActive);
-      }//end movies if
-      else if(cat == "shows"){
-        //Toggle the musicActive button, and save its state in localStorage
+      }
+      //Toggle the musicActive button, and save its state in localStorage
+      else if(section == "shows"){
         $scope.showsActive = !$scope.showsActive;
         localStorage.setItem("show_shows", $scope.showsActive);
       }//end shows if
     };//end toggleMusic
 
   });//end Buttons
-
 })();
+*/
