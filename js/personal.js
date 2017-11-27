40202217d67c33a00ba641e98388427e2e6f1b21
Sam Wang <yuw160@ucsd.edu>
2017-11-27 01:53:45 -0800

    personal info data submission working

--- js/personal.js
+++ js/personal.js
@@ -140,6 +140,42 @@ function getUserData(path, field) {
   });
 };
 
+function submitForm() {
+  var form = document.getElementById('personal_form');
+  //checks to see that the user entered the data correctly
+  if(form.checkValidity()) {
+    //only submits data after confirming with the user
+    if(confirm("Are you sure this is the memory you want to keep?")) {
+      //creates variables for each part of the memory
+      var year = document.getElementById('year').value;
+      var title = document.getElementById('title').value;
+      var description = document.getElementById('description').value;
+      //attempts to submit the data to database
+      try {
+      writeUserData('/personal_memories' + '/' + year + '/' + title, 'year', year);
+      writeUserData('/personal_memories' + '/' + year + '/' + title, 'title', title);
+      writeUserData('/personal_memories' + '/' + year + '/' + title, 'description', description);
+      //logs
+      console.log('personal memory form submitted');
+      alert('Memory Submitted!');
+      //form.submit();
+      }
+      //logs error in case data submission failed
+      catch(e) {
+        console.log(e);
+      }
+    }
+    //in case the user cancels the memory submission
+    else {
+      console.log('personal memory form not submitted');
+    }
+  }
+  //in case the user did not enter the data correctly
+  else {
+    document.getElementById('submit_handler').click();
+  }
+};
+
 /****************************** Old Code ******************************/
 
 /*
